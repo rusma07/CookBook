574cb28200bb9661dabd428267873fdbbb4db842
Rushma Shrestha <135934869+rusma07@users.noreply.github.com>
2023-08-23 20:16:58 +0545

    Add files via upload

---
+++ service.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './service.css'
+
+export default function Service() {
+  return (
+    <>
+   
+    <h1>Our Services</h1>
+    <p id='p'>With a promise to serve you the best</p>
+    <div className='flexbox'>
+    <div className="bod">
+    <div className="box1">
+    <p>Fresh And Organic</p>
+      <img src="https://th.bing.com/th/id/R.196b1d34cf24aa7de665f5e0af46f7a8?rik=fwsCyLu%2fhOFV1w&pid=ImgRaw&r=0" alt="fresh fruits" />
+      
+      <h5>lasdkfndsl dfonsdofn sdfnsdffj dsiov  dsaoij sdif asdif cx zodshf asdof xo asodfh xocvhas ocxvh so zodvj laskdjo soi df ow oasdf d asdkhkghogiv  kzx cvohf d fl dsfloa odif asdkf.</h5>
+      <button> Learn more </button>
+    </div>
+    
+    </div>
+    <div className="bod">
+    <div className="box2">
+      <p>Free Delivery</p>
+      <img src="https://th.bing.com/th/id/R.f229a1322f3cc743a93e36492d7f34ab?rik=vXcG4n7u7RyIKw&pid=ImgRaw&r=0&sres=1&sresct=1" alt="" />
+      <h5>lasdkfndsl dfonsdofn soij sdif asdif cx zodshf asdof xo asodfh xocvhas ocxvh so zodvj laskdjo soi df ow oasdf d asdkhkghal laksdjf askdj ogiv  kzx cvohf d fl dsfloa odif asdkf.</h5>
+      <button> Learn more</button>
+      
+    </div>
+    
+    </div>
+    <div className="bod">
+    <div className="box3">
+      <p>Easy Payment Methods</p>
+      <img src="https://hatinco.com/images/imagesdnbscreen/BIG-CORPORATIONS.png" alt="" />
+      <h5>lasdkfndsl dfonsdofn sdfnsdffj dsiov  dsaoij sdif asdif cx zodshf asdof xo asodfh xocvhas ocxvh so zodvj laskdjo soi df ow oasdf d asdkhkghal laksd cvohf d fl dsfloa odif asdkf.</h5>
+      <button> Learn more</button>
+    </div>
+    
+    </div>
+    </div>
+  
+      
+
+    </>
+  )
+
+}
